971aabf7987d97ce43de3e413e1a67265720baef
CwaniX <cwanix@cwanix.pl>
2019-05-10 08:26:08 +0200

    Empty project of game server

---
+++ game-server/src/main/java/pl/cwanix/opensun/gameserver/GameServerApplication.java
@@ -0,0 +1,13 @@
+package pl.cwanix.opensun.gameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServerApplication.class, args);
+	}
+
+}
