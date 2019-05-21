de44c68f2fb6d1b541ce077f004509db70df8da9
CwaniX <cwanix@cwanix.pl>
2019-05-21 13:48:40 +0200

    Agent server project

---
+++ agent-server/src/main/java/pl/cwanix/opensun/agentserver/AgentServerApplication.java
@@ -0,0 +1,13 @@
+package pl.cwanix.opensun.agentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentServerApplication.class, args);
+	}
+
+}
