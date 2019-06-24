54d64e8cebb90ad8eac6d7805dfb77ac7901d4ac
CwaniX <cwanix@cwanix.pl>
2019-06-24 09:52:10 +0200

    World server project

---
+++ world-server/src/main/java/pl/cwanix/opensun/worldserver/WorldServerApplication.java
@@ -0,0 +1,13 @@
+package pl.cwanix.opensun.worldserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldServerApplication.class, args);
+	}
+
+}
