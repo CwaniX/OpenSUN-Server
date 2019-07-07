01969d7be5fe47165b14b56efca1d06b7fa26bb1
CwaniX <mccwanix@gmail.com>
2019-07-07 23:13:02 +0200

    Handling unknown packet error

--- agent-server/src/main/java/pl/cwanix/opensun/agentserver/packets/c2s/unknown/C2SEA09Packet.java
+++ agent-server/src/main/java/pl/cwanix/opensun/agentserver/packets/c2s/unknown/C2SEA09Packet.java
@@ -1,8 +1,5 @@
 package pl.cwanix.opensun.agentserver.packets.c2s.unknown;
 
-import io.netty.channel.ChannelHandlerContext;
-import pl.cwanix.opensun.agentserver.packets.s2c.status.S2CAnsRecoverAttrMpPacket;
-import pl.cwanix.opensun.agentserver.packets.s2c.status.S2CAnsRecoverAttrPacket;
 import pl.cwanix.opensun.commonserver.packets.IncomingPacket;
 import pl.cwanix.opensun.commonserver.packets.Packet;
 import pl.cwanix.opensun.commonserver.packets.PacketCategory;
@@ -12,12 +9,6 @@ public class C2SEA09Packet implements Packet {
 	
 	public C2SEA09Packet(byte[] value) {
 		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void process(ChannelHandlerContext ctx) {
-		ctx.writeAndFlush(new S2CAnsRecoverAttrPacket());
-		ctx.writeAndFlush(new S2CAnsRecoverAttrMpPacket());
 	}
 
 }
