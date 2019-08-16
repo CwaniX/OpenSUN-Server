66c5883c10116861ae7d55e123ad64d84c42a4f0
CwaniX <mccwanix@gmail.com>
2019-08-16 16:09:18 +0200

    Packet fix

--- world-server/src/main/java/pl/cwanix/opensun/worldserver/packets/c2s/C2S483CPacket.java
+++ world-server/src/main/java/pl/cwanix/opensun/worldserver/packets/c2s/C2S483CPacket.java
@@ -3,9 +3,10 @@ package pl.cwanix.opensun.worldserver.packets.c2s;
 import pl.cwanix.opensun.commonserver.packets.IncomingPacket;
 import pl.cwanix.opensun.commonserver.packets.Packet;
 import pl.cwanix.opensun.commonserver.packets.PacketCategory;
+import pl.cwanix.opensun.worldserver.server.context.WorldServerContext;
 
 @IncomingPacket(category = PacketCategory.CONNECTION, type = (byte) 0x3C)
-public class C2S483CPacket implements Packet {
+public class C2S483CPacket implements Packet<WorldServerContext> {
 	
 	public C2S483CPacket(byte[] value) {
 		// TODO Auto-generated constructor stub
