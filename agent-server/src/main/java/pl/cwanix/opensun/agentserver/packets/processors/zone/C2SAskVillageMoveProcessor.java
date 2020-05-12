7445a21efb180454c19911dd3dfea34b0081869b
CwaniX <mccwanix@gmail.com>
2020-05-12 14:31:48 +0200

    Moving to Zapir Lake

--- agent-server/src/main/java/pl/cwanix/opensun/agentserver/packets/processors/zone/C2SAskVillageMoveProcessor.java
+++ agent-server/src/main/java/pl/cwanix/opensun/agentserver/packets/processors/zone/C2SAskVillageMoveProcessor.java
@@ -3,9 +3,14 @@ package pl.cwanix.opensun.agentserver.packets.processors.zone;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import pl.cwanix.opensun.agentserver.entities.CharacterEntity;
 import pl.cwanix.opensun.agentserver.packets.c2s.zone.C2SAskVillageMovePacket;
+import pl.cwanix.opensun.agentserver.packets.s2c.sync.S2CAnsAllPlayersEquipInfoPacket;
+import pl.cwanix.opensun.agentserver.packets.s2c.sync.S2CAnsAllPlayersGuildInfoPacket;
 import pl.cwanix.opensun.agentserver.packets.s2c.sync.S2CAnsPlayerEnterPacket;
 import pl.cwanix.opensun.agentserver.packets.s2c.zone.S2CAnsVillageMovePacket;
+import pl.cwanix.opensun.agentserver.server.AgentServerChannelHandler;
+import pl.cwanix.opensun.agentserver.server.session.AgentServerSession;
 import pl.cwanix.opensun.commonserver.packets.SUNPacketProcessor;
 import pl.cwanix.opensun.commonserver.packets.annotations.PacketProcessor;
 
@@ -16,6 +21,14 @@ public class C2SAskVillageMoveProcessor implements SUNPacketProcessor<C2SAskVill
 
     @Override
     public void process(ChannelHandlerContext ctx, C2SAskVillageMovePacket packet) {
+        AgentServerSession session = ctx.channel().attr(AgentServerChannelHandler.SESSION_ATTRIBUTE).get();
+
+        CharacterEntity character = session.getCharacter();
+        character.getPosition().setRegion(20004);
+        character.getPosition().setLocationX(169.90472F);
+        character.getPosition().setLocationY(267.42834F);
+        character.getPosition().setLocationZ(33.776058F);
+
         ctx.writeAndFlush(new S2CAnsVillageMovePacket());
     }
 }
